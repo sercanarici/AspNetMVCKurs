a7baa348cfdf95804422063d1722e703674b8ed8
sercanarici <sercanarici@hotmail.com>
2019-12-24 20:45:14 +0300

    Hafta2_2 eklendi

---
+++ Hafta2_2/RazorVeHtmlHelper/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using RazorVeHtmlHelper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorVeHtmlHelper.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<SelectListItem> liste = new List<SelectListItem>();
+            liste.Add(new SelectListItem { Text = "Istanbul", Value = "34" });
+            liste.Add(new SelectListItem { Text = "Ankara", Value = "34" });
+            liste.Add(new SelectListItem { Text = "Bursa", Value = "16" });
+            liste.Add(new SelectListItem { Text = "Trabzon", Value = "34" });
+
+            ViewBag.Iller = liste;
+
+            return View();
+        }
+
+        public ActionResult Reklam()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Hafta2_2/RazorVeHtmlHelper/Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RazorVeHtmlHelper.Models
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name ="Doğum Tarihi")]
+        public DateTime DOB { get; set; }
+    }
+}
