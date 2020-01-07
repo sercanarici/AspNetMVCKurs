eceeae7e1b71d65fc1185cfda777ac55fc2b2f67
Lab03-16 <Lab03-16@LAB08-14>
2020-01-07 20:56:23 +0300

    hafta3_2 eklendi

---
+++ Hafta3_2/Alcom/Models/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Alcom.Models
+{
+    public class ProductViewModel
+    {
+        public string Baslik { get; set; }
+
+        public List<string> Products { get; set; }
+    }
+}
