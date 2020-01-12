9dafe3e6032c9f9699144aea01708b5915b8eb63
R.Sercan Arıcı <sercanarici@hotmail.com>
2020-01-13 01:37:04 +0300

    product ve product details sayfaları eklendi

---
+++ Hafta3_2/Alcom/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Alcom.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
+    }
+}
