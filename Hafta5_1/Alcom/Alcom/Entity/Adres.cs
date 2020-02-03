ce21bbdd7b30c924ba1621bf530684e76c7a413c
sercanarici <>
2020-02-03 20:54:52 +0300

    Hafta5  eklendi

---
+++ Hafta5_1/Alcom/Alcom/Entity/Adres.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Alcom.Entity
+{
+    public class Adres : Base
+    {
+        public int KullaniciId { get; set; }
+
+        public int IlceId { get; set; }
+        [Required]
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "Adres 2-200 karakter aralığında olmalıdır.")]
+        public string Aciklama { get; set; }
+
+
+        [ForeignKey("IlceId")]
+        public virtual Ilce Ilcesi { get; set; }
+
+        [ForeignKey("KullaniciId")]
+        public virtual Kullanici AdresKullanicisi { get; set; }
+    }
+}
---
+++ Hafta5_1/Alcom/Alcom/Entity/Il.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Alcom.Entity
+{
+    public class Il : Base
+    {
+        public Il()
+        {
+            this.Ilceleri = new HashSet<Ilce>();
+        }
+
+        [Required]
+        public string Adi { get; set; }
+        public int PlakaKodu { get; set; }
+
+        public virtual ICollection<Ilce> Ilceleri{ get; set; }
+    }
+}
